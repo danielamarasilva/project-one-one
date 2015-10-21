9a6d36435c46b658c332716a2b693aa7d1005263
danielamara <danielamaradms@gmail.com>
2015-10-21 00:22:07 -0200

    Atualização

---
+++ SiteAutonove/js/financiamentoController.js
@@ -0,0 +1,43 @@
+﻿
+var FinanciamentoController = {
+
+    init: function () {
+        FinanciamentoController.setForm();
+    },
+
+    setForm: function () {
+        var form = document.querySelector('form');
+        form.addEventListener('submit', function (event) {
+            FinanciamentoController.sendForm();
+            event.preventDefault();
+        });
+    },
+
+    sendForm: function () {
+        var dadosForm = $("form").serialize();
+
+        $.ajax({
+            url: '',
+            data: dadosForm,
+
+            success: function (result) {
+                FinanciamentoController.showResult(result, 1);
+            },
+            error: function () {
+                FinanciamentoController.showResult(result, 2);
+            }
+        });
+    },
+
+    showResult: function (result, flag) {
+        if(flag === 1)
+        {
+            alert("Sucesso!!");
+        }
+        else
+            alert("Errroooo!!");
+    }
+}
+
+//inicialização
+FinanciamentoController.init();
